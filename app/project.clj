42d8cf5c414bdc0241bee865fc15d2d9f23c9f06
tomer <tomer.g2@gmail.com>
2023-11-12 14:26:37 +0200

    install basic deps

--- app/project.clj
+++ app/project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [http-kit "2.6.0"]
+                 [ring/ring-core "1.9.5"]
+                 [ring/ring-json "0.5.1"]
+                 [ring/ring-mock "0.4.0"]]
   :main ^:skip-aot app.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
