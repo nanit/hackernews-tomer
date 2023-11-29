8adebea8d001a6925bcb3e6c4986dc3b6c69cfc5
tomer <tomer.g2@gmail.com>
2023-11-29 06:56:02 +0200

    add port and print url

--- app/src/app/core.clj
+++ app/src/app/core.clj
@@ -7,6 +7,8 @@
   (response {:status 200
              :headers {"Content-Type" "application/json"}
              :body "{\"body\": \"pong\"}"}))
-
+(def port 8000)
 (defn -main []
-  (run-server (params/wrap-params ping-handler) {:port 8080}))
+  (println (str "server running on port " port))
+  (println (str "ping url http://localhost:" port))
+  (run-server (params/wrap-params ping-handler) {:port port}))
