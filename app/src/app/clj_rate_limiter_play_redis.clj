f9a46d474aa7a862546063d8bc4e7fe1b75f93c0
tomer <tomer.g2@gmail.com>
2023-12-11 11:41:26 +0200

    redis based not working

---
+++ app/src/app/clj_rate_limiter_play_redis.clj
@@ -0,0 +1,40 @@
+(ns app.clj-rate-limiter-play-redis
+  (:require
+    [clj-rate-limiter.core :as r]
+    [org.httpkit.server :refer [run-server]]
+    [compojure.core :refer [GET wrap-routes routes defroutes]]
+    [taoensso.timbre :refer [info]]
+    [ring.util.response :refer [response]]))
+
+(def redis {:pool {}
+            :spec {:uri ""}})
+
+(def limiter (r/create
+               (r/rate-limiter-factory :redis
+                                       :redis redis
+                                       :namespace "APIs"
+                                       :interval 1000
+                                       :max-in-interval 100)))
+
+(defn check-email-middleware [handler]
+  (fn [request]
+    (let [allow (r/allow? limiter "key1")]
+      (if (= allow false)
+        (response {:status 429})
+        (handler request)))))
+
+(defroutes limit-route
+           (wrap-routes
+             (routes (GET "/limit" [] "limit"))
+             check-email-middleware))
+
+(defroutes no-limit-route
+           (GET "/no-limit" [] "no-limit"))
+
+(defroutes app
+           limit-route
+           no-limit-route)
+
+(defn -main []
+  (run-server app {:port 3000})
+  (info "Server is running" {:port 3000}))
