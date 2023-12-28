3a0536fdc209af6f5c00ba9e7b803d7b197a5270
tomer <tomer.g2@gmail.com>
2023-12-28 07:16:36 +0200

    static web local storage working

--- app/src/app/io_staticweb_rate_limit_play.clj
+++ app/src/app/io_staticweb_rate_limit_play.clj
@@ -1,16 +1,20 @@
-(ns app.io-staticweb-rate-limit-play
-  (:require [org.httpkit.server :refer [run-server]]
-            [compojure.core :refer [GET]]
-            [taoensso.timbre :refer [info]]
-            [io.staticweb.rate-limit.middleware :refer [wrap-rate-limit ip-rate-limit]]
-            [io.staticweb.rate-limit.storage :as storage]
-            [io.staticweb.rate-limit.redis :as r-storage]))
+(ns app.io-staticweb-rate-limit-play)
 (use 'compojure.core)
+(require '[io.staticweb.rate-limit.middleware :refer [ip-rate-limit wrap-rate-limit]])
+(require '[io.staticweb.rate-limit.storage :as storage])
+(require '[org.httpkit.server :refer [run-server]])
+
+;; Instantiate a storage backend
 (def storage (storage/local-storage))
-;(def rstr (r-storage/redis-storage))
-(def limit (ip-rate-limit :limit-id 1 (java.time.Duration/ofSeconds 1)))
+
+;; Define the rate limit: 1 req/s per IP address
+(def limit (ip-rate-limit :limit-id 1 (java.time.Duration/ofSeconds 10)))
+
+;; Define the middleware configuration
 (def rate-limit-config {:storage storage :limit limit})
-(def app (
+
+;; Wrap the /limit route in the rate limiting middleware
+(def app (routes
            (GET "/no-limit" [] "no-limit")
            (wrap-rate-limit
              (GET "/limit" [] "limit")
@@ -18,4 +22,4 @@
 
 (defn -main []
   (run-server app {:port 3000})
-  (info "Server is running" {:port 3000}))
+  (println "Server is running" {:port 3000}))
