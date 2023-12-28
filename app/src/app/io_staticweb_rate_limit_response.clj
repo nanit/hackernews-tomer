26a8c81335c68e122cba6151142d3dfbdf03f90c
tomer <tomer.g2@gmail.com>
2023-12-28 18:47:07 +0200

    todo

--- app/src/app/io_staticweb_rate_limit_response.clj
+++ app/src/app/io_staticweb_rate_limit_response.clj
@@ -6,3 +6,15 @@
    :headers {"Retry-After" (str (.getSeconds retry-after))
              "Quota" quota}
    :status 429})
+
+; TODO figure this out
+;(testing "with custom response"
+;         (let [custom-rsp {:headers {"Content-Type" "text/plain"}
+;                           :body "Hello, World!"
+;                           :status 418}
+;               rsp (r/too-many-requests-response custom-rsp (Duration/ofSeconds 5))
+;               headers (:headers rsp)]
+;           (is (= (:status rsp) 418))
+;           (is (= (:body rsp) "Hello, World!"))
+;           (is (= (headers "Content-Type") "text/plain"))
+;           (is (= (headers "Retry-After") "5"))))))
